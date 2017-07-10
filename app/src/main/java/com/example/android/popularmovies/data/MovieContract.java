b328011948523be5d3f8885feb03dcb2995ddd9f
a3y3 <sohamssd@gmail.com>
2017-07-10 19:27:45 +0530

    Bug Fixes

---
+++ app/src/main/java/com/example/android/popularmovies/data/MovieContract.java
@@ -0,0 +1,13 @@
+package com.example.android.popularmovies.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by Soham on 10-Jul-17.
+ */
+
+public class MovieContract{
+    public static final class MovieEntry implements BaseColumns{
+        public static final String TABLE_NAME="favourite_movies";
+    }
+}
